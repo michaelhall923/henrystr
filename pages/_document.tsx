fe87241adbb4acc613003fa6024122c13625950d
Michael Hall <michaelhall923@protonmail.com>
2023-07-12 01:58:53 -0400

    initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en" className="bg-chalk text-tree dark:bg-charcoal ">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="px-4 pt-4">
+      <div className="flex text-3xl font-bold">henrystr</div>
+    </div>
+  );
+}
